3c15bb16d3d9b079533ee2dce44a70da16e90ad5
Robert R Evans <robert@codewranglers.org>
2011-05-01 23:07:06 -0700

    another small bug fix. benedryl and coding == bugs

--- lib/ti/generate/view.rb
+++ lib/ti/generate/view.rb
@@ -6,7 +6,7 @@ module Ti
         
         def create(name, options={})
           create_new_file("app/views/#{name}.coffee")
-          create_new_file("spec/views/#{name}_spec.coffee", File.read(::Ti::ROOT_PATH.join("ti/templates/specs/app_spec.coffee")))
+          create_new_file("specs/views/#{name}_spec.coffee", File.read(::Ti::ROOT_PATH.join("ti/templates/specs/app_spec.coffee")))
         end
         
         def location
