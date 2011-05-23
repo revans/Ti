fc69361f58b92f75efdee9a43ddda6e2ebeaeaca
Robert R Evans <robert@codewranglers.org>
2011-05-23 14:57:17 -0700

    CoffeeScript compilation is added and version updated.

--- lib/ti/compiler/coffee_scripts.rb
+++ lib/ti/compiler/coffee_scripts.rb
@@ -1,3 +1,4 @@
+require 'coffee_script'
 module Ti
   module Compiler
     class CoffeeScripts
@@ -5,15 +6,21 @@ module Ti
         include ::Ti::Utils
         
         def compile_all!
-          coffeefile  = File.read(base_location.join('Coffeefile')).split('\n').compact.delete_if { |l| l.include?("#") }
-          files       = coffeefile.collect { |a| Dir.glob[a] }.flatten!.uniq
+          coffeefile  = File.read(base_location.join('Coffeefile')).split("\n").compact.delete_if { |l| l.include?("#") }
+          files       = coffeefile.collect { |a| Dir.glob(coffeefile) }.flatten!
+          if files.nil?
+            log "There are no files to compile."
+            exit(0)
+          end
           @contents   = ''
-          files.each { |f| @contents << File.read(f) }
-          compile(@contents, base_location.join("Resources/#{get_app_name}.js"))
+          files.uniq!.each { |f| @contents << File.read(f) }
+          compile_location = "Resources/#{underscore(get_app_name).downcase}.js"
+          compile(@contents, base_location.join(compile_location))
+          log "Your CoffeeScripts have been compiled to: #{compile_location}"
         end
         
         def compile(contents, compile_to_location)
-          coffeescript  = CoffeeScript.compile(contents, :no_wrap => false)
+          coffeescript  = ::CoffeeScript.compile(contents, :no_wrap => false)
           File.open(compile_to_location, 'w') { |f| f.write(coffeescript) }
         end
         
