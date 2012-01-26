7e9b6ce574ce3c6d06d882a91e0c0af61d919c1e
David Long <dave@davejlong.com>
2012-01-26 14:36:54 -0500

    Added support for other builders besides iphone

--- lib/ti/builder/titanium.rb
+++ lib/ti/builder/titanium.rb
@@ -11,7 +11,17 @@ module Ti
 
         def build(platform)
           log "Building with Titanium... DEVICE_TYPE: #{platform}"
-          sh %Q{bash -c "#{TI_BUILD} run #{PROJECT_ROOT}/ #{IPHONE_SDK_VERSION} #{APP_ID} #{APP_NAME} #{APP_DEVICE}" | perl -pe 's/^\\[DEBUG\\].*$/\\e[35m$&\\e[0m/g;s/^\\[INFO\\].*$/\\e[36m$&\\e[0m/g;s/^\\[WARN\\].*$/\\e[33m$&\\e[0m/g;s/^\\[ERROR\\].*$/\\e[31m$&\\e[0m/g;'}
+		  
+		  if platform == "android"
+			sdk = ANDROID_SDK_VERSION
+			builder = "#{TI_ANDROID_DIR}/builder.py"
+		  elsif platform == "iphone" || platform == "ipad"
+			sdk = IPHONE_SDK_VERSION
+			builder = "#{TI_IPHONE_DIR}/builder.py"
+		  end
+		  
+		  builder = "#{TI_ASSETS_DIR}/#{platform}/builder.py"
+          sh %Q{bash -c "#{builder} run #{PROJECT_ROOT}/ #{sdk} #{APP_ID} #{APP_NAME} #{APP_DEVICE}" | perl -pe 's/^\\[DEBUG\\].*$/\\e[35m$&\\e[0m/g;s/^\\[INFO\\].*$/\\e[36m$&\\e[0m/g;s/^\\[WARN\\].*$/\\e[33m$&\\e[0m/g;s/^\\[ERROR\\].*$/\\e[31m$&\\e[0m/g;'}
         end
       end
     end
